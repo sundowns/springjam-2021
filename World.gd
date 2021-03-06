8af4500a5cc5990c3dc6574315c74f8859d2f2f1
Tom <tom@smallridge.com.au>
2021-03-06 15:35:08 +0800

    placing plants via schematics

--- World.gd
+++ World.gd
@@ -2,15 +2,20 @@ extends Spatial
 
 onready var camera: Camera = $Camera
 onready var selection_tool: MeshInstance = $SelectionTool/MeshInstance
+onready var plants_container: Spatial = $PlantsContainer
 onready var ray: RayCast = $SelectionTool/RayCast
 onready var map: GridMap = $GridMap
+
+export(PackedScene) var sunflower_schematic_scene: PackedScene
 
 export(Color, RGB) var valid_selection: Color
 export(Color, RGB) var invalid_selection: Color
 
+var selection_position: Vector3 = Vector3.ZERO
 var in_menu := false
 var can_build := false
 var mouse_pos: Vector2
+var map_point: Vector3
 const ray_length = 1000
 
 func place_building(building: Plant, grid_indices: Vector3):
@@ -25,12 +30,15 @@ func _physics_process(_delta):
 		ray.cast_to = to
 		ray.force_raycast_update()
 		
-		var map_point = map.world_to_map(ray.get_collision_point())
+		map_point = map.world_to_map(ray.get_collision_point())
+# warning-ignore:narrowing_conversion
+# warning-ignore:narrowing_conversion
+# warning-ignore:narrowing_conversion
 		var tile = map.get_cell_item(map_point.x, map_point.y, map_point.z) 
-		print(map_point)
+#		print(tile)
 		
 		# Its an empty tile (need to make a flat blank, empty tile in index 0 of the meshlib)
-		if tile == 0:
+		if tile == -1:
 			selection_tool.get_surface_material(0).set_shader_param("albedo", valid_selection)
 			can_build = true
 		else:
@@ -38,5 +46,22 @@ func _physics_process(_delta):
 			can_build = false
 		
 		# Move selection tool mesh
-		var selection_position = map.map_to_world(map_point.x, map_point.y, map_point.z)
+# warning-ignore:narrowing_conversion
+# warning-ignore:narrowing_conversion
+# warning-ignore:narrowing_conversion
+		selection_position = map.map_to_world(map_point.x, map_point.y, map_point.z)
 		selection_tool.global_transform.origin = selection_position
+		
+		if Input.is_action_just_pressed("build") and can_build:
+			place_schematic(0)
+
+func place_schematic(id):
+	match id:
+		0:
+			var new_plant = sunflower_schematic_scene.instance()
+			plants_container.add_child(new_plant)
+			new_plant.global_transform.origin = selection_position
+# warning-ignore:narrowing_conversion
+# warning-ignore:narrowing_conversion
+# warning-ignore:narrowing_conversion
+			map.set_cell_item(map_point.x, map_point.y, map_point.z, 1)
--- schematics/Schematic.gd
+++ schematics/Schematic.gd
@@ -1,9 +1,20 @@
 extends Spatial
 
 export(PackedScene) var scene: PackedScene
+export(float) var build_time := 5.0
 
-# TODO: build over time...
+onready var build_timer: Timer = $BuildTimer
 
-func place_at(grid_indices: Vector3):
-	pass
-	# TODO: replace self with actual scene
+func _ready():
+	build_timer.start(build_time)
+
+func _on_BuildTimer_timeout():
+	spawn_plant()
+
+func spawn_plant():
+	build_timer.disconnect("timeout", self, "_on_BuildTimer_timeout")
+	var new_plant = scene.instance()
+	var old_position = global_transform.origin
+	replace_by(new_plant)
+	new_plant.global_transform.origin = old_position
+	queue_free()
